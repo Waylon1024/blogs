0131c9f8e88a97f65d91d9aeda05d5cff1f731f2
Waylon <945518379@qq.com>
2023-05-12 07:27:27 +0000

    修订：查看更多按钮居中
    Signed-off-by: Waylon <945518379@qq.com>

--- blog-view/src/components/blog/infoMore.vue
+++ blog-view/src/components/blog/infoMore.vue
@@ -1,14 +1,16 @@
 <template>
-<div>
-    <button href="javascript:;" @click.prevent="goToMore" class="color-btn">查看更多</button>
-</div>
+  <div>
+    <div class="button-container">
+      <button href="javascript:;" @click.prevent="goToMore" class="color-btn">查看更多</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  methods:{
-    goToMore(){
-      this.$router.push({name:'blogAll'})
+  methods: {
+    goToMore() {
+      this.$router.push({name: 'blogAll'})
     }
   }
 }
