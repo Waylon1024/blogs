30bf0fa9f3ec532225142190bfacd1417ae86e8a
Waylon <945518379@qq.com>
2023-05-13 15:36:12 +0800

    更改：预览图url改为上传本地图片

--- blog-cms/src/views/blog/blog/WriteBlog.vue
+++ blog-cms/src/views/blog/blog/WriteBlog.vue
@@ -165,16 +165,16 @@
         this.imageUrl = URL.createObjectURL(file.raw);
       },
       beforeAvatarUpload(file) {
-        const isImage = /\.(jpg|jpeg|png)$/.test(file.type.toLowerCase());
+        const isJPG = file.type === 'image/jpeg';
         const isLt2M = file.size / 1024 / 1024 < 2;
 
-        if (!isImage) {
-          this.$message.error('上传头像图片只能是 JPG、JPEG 或 PNG 格式!');
+        if (!isJPG) {
+          this.$message.error('上传头像图片只能是 JPG 格式!');
         }
         if (!isLt2M) {
           this.$message.error('上传头像图片大小不能超过 2MB!');
         }
-        return isImage && isLt2M;
+        return isJPG && isLt2M;
       },
 			getData() {
 				getCategoryAndTag().then(res => {
